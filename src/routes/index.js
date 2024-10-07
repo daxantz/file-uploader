de8be3341e83776ec295acf08ea2bd77db9a3714
daxantz <daxantz@gmail.com>
2024-10-07 18:48:47 -0400

    used prisma to return a user with the users folders and files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,33 @@
 const { Router } = require("express");
 const indexRouter = Router();
 const { checkAuthenticated } = require("../middleware/auth");
+const { prisma } = require("../passport-config");
 
 indexRouter.get("/", checkAuthenticated, async (req, res) => {
-  res.render("index", { user: req.user });
+  try {
+    const folders = await prisma.folder.findMany({
+      where: {
+        userId: req.user.id,
+      },
+    });
+
+    const files = await prisma.file.findMany({
+      where: { userId: req.user.id },
+    });
+    const userWithFilesAndFolders = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      include: {
+        files: true,
+        folders: true,
+      },
+    });
+    console.log(userWithFilesAndFolders);
+    res.render("index", { user: userWithFilesAndFolders });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = indexRouter;
