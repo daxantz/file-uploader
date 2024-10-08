aba65901622a191ba03e85d50296f4a16de80587
daxantz <daxantz@gmail.com>
2024-10-08 12:32:19 -0400

    added logic for getting the current folders sub-folders and files

--- src/controllers/folder.js
+++ src/controllers/folder.js
@@ -2,17 +2,21 @@ const { prisma } = require("../passport-config");
 exports.folderPageGet = async (req, res) => {
   try {
     const { id } = req.params;
-    const currentFolder = await prisma.folder.findUnique({
+    //finds specific folder and all its files/folders
+    const currentFolderWithFiles = await prisma.folder.findUnique({
       where: {
         id: Number(id),
       },
+      include: { files: true },
     });
-
+    //returns all sub-folders of the current folder
     const currentFolders = await prisma.folder.findMany({
       where: { parentFolderId: Number(id) },
+      include: { files: true },
     });
+    console.log(currentFolderWithFiles);
     res.render("folder", {
-      folder: currentFolder,
+      folder: currentFolderWithFiles,
       currentFolders: currentFolders,
     });
   } catch (error) {
@@ -33,6 +37,20 @@ exports.folderPut = async (req, res) => {
     });
     console.log("folder added");
     res.redirect(`/storage/${req.params.id}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.folderPatch = async (req, res) => {
+  try {
+    const { newName } = req.body;
+    const folder = await prisma.folder.update({
+      where: { id: Number(req.params.id) },
+      data: { name: newName },
+    });
+    console.log(`${folder} new name is ${newName}`);
+    res.redirect(`/storage/${folder.parentFolderId}`);
   } catch (error) {
     console.log(error);
   }
