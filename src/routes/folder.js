e72fca87c43f2a131aade36874defa30866bf6e9
daxantz <daxantz@gmail.com>
2024-10-07 21:22:40 -0400

    both endpoints now use the id of the current folder

--- src/routes/folder.js
+++ src/routes/folder.js
@@ -2,9 +2,13 @@ const { Router } = require("express");
 const router = Router();
 const auth = require("../middleware/auth");
 const folderController = require("../controllers/folder");
-
+router.get(
+  "/storage/:id",
+  auth.checkAuthenticated,
+  folderController.folderPageGet
+);
 router.post(
-  "/create-folder",
+  "/create-folder/:id",
   auth.checkAuthenticated,
   folderController.folderPut
 );
