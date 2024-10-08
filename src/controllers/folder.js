c3706e1424f9143533fd09179ce9e9723bbc7d7c
daxantz <daxantz@gmail.com>
2024-10-07 21:26:36 -0400

    added handler to get and render pages for each folder and added parentfolderId when making a new sub folder

--- src/controllers/folder.js
+++ src/controllers/folder.js
@@ -1,14 +1,38 @@
 const { prisma } = require("../passport-config");
+exports.folderPageGet = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const currentFolder = await prisma.folder.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
 
+    const currentFolders = await prisma.folder.findMany({
+      where: { parentFolderId: Number(id) },
+    });
+    res.render("folder", {
+      folder: currentFolder,
+      currentFolders: currentFolders,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 exports.folderPut = async (req, res) => {
   try {
+    const rootFolder = await prisma.folder.findFirst({
+      where: { name: "root", userId: req.user.id },
+    });
     await prisma.folder.create({
       data: {
         name: "new folder",
         userId: req.user.id,
+        parentFolderId: Number(req.params.id),
       },
     });
-    console.log(`${req.user} (id: ${req.user.id}) has created a new folder`);
+    console.log("folder added");
+    res.redirect(`/storage/${req.params.id}`);
   } catch (error) {
     console.log(error);
   }
