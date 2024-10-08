6b1cad759307ce3a385863c983e58c080ca639b0
daxantz <daxantz@gmail.com>
2024-10-08 12:24:30 -0400

    endpoint now takes an id parameter when creating files

--- src/routes/file.js
+++ src/routes/file.js
@@ -7,7 +7,7 @@ const fileController = require("../controllers/file");
 
 router.get("/upload-file", auth.checkAuthenticated, fileController.fileFormGet);
 router.post(
-  "/upload-file",
+  "/upload-file/:id",
   upload.single("uploaded_file"),
   fileController.filePut
 );
