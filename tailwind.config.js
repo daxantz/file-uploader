ab163827ebe4721f245ad62504a3afbfabff3765
daxantz <daxantz@gmail.com>
2024-10-15 20:26:51 -0400

    sets default background color to white when using daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ejs}", "./src/views/**/*.ejs"],
+  daisyui: {
+    styled: true,
+    themes: [
+      "light", // You can specify predefined themes directly
+      // You can also customize the dark theme like this:
+      {
+        mydark: {
+          primary: "#570df8", // Customize primary color
+          "base-100": "#ffffff", // Set your desired background color
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
